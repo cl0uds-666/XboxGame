82385d0fa11500431a8bdfecbf4eed4c9032e8c7
cl0uds-666 <codeywilson005@gmail.com>
2026-01-13 01:09:29 +0000

    Menu Added
    polish things off

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject creditsPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1); // your game scene index
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QuitGame called!");
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    public void OpenCredits()
+    {
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+}
